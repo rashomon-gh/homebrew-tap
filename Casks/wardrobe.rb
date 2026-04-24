d334bd005f555cb2c8c5eefcfec2262e65c68f72
rashomon <shawon.ashraf@outlook.com>
2026-04-24 15:48:30 +0200

    updated wardrobe

--- Casks/wardrobe.rb
+++ Casks/wardrobe.rb
@@ -1,5 +1,5 @@
 cask "wardrobe" do
-  version "0.0.1" # Replace with the actual release version
+  version "0.0.2"
   sha256 :no_check 
 
   url "https://github.com/rashomon-gh/Wardrobe/releases/download/#{version}/Wardrobe.zip"
